f0c1bd4faaec8cc0e6065250fe1727535f7d413b
Bencro1 <benjamin.crot@students.hevs.ch>
2024-12-11 09:16:14 +0100

    fonction update

--- src/ClientServer_Communication/Request/RequestHandlerFactory.java
+++ src/ClientServer_Communication/Request/RequestHandlerFactory.java
@@ -4,10 +4,10 @@ import roadnetwork.Graph;
 
 public class RequestHandlerFactory {
     public static RequestHandler getHandler(String requestType, Graph graph) {
-        return switch (requestType) {
-            case "UPDATE" -> new UpdateHandler();
+        return switch (requestType.toUpperCase()) {
+            case "UPDATE" -> new UpdateHandler(graph); // Passez le graphe ici
             case "PATH" -> new PathCalculationHandler(graph);
-            default -> throw new IllegalArgumentException("Unknown request type");
+            default -> throw new IllegalArgumentException("Unknown request type: " + requestType);
         };
     }
 }
